3bb718018fb13d084fd29662162d77eaf1aad5cf
SPRINTYUZMEOKULU <sprintyuzmeokulu@gmail.com>
2026-09-03 21:49:13 +0300

    Öğrenci veli bilgi merkezini dosya düzenine bağla

--- app/ogrenciler/[id]/layout.tsx
+++ app/ogrenciler/[id]/layout.tsx
@@ -1,5 +1,6 @@
 import type { ReactNode } from "react";
 import StudentFileTabs from "./student-file-tabs";
+import StudentProfileCenter from "./student-profile-center";
 import "./student-file-tabs.css";
 
 export default function StudentFileLayout({ children }: { children: ReactNode }) {
@@ -7,6 +8,7 @@ export default function StudentFileLayout({ children }: { children: ReactNode })
     <>
       {children}
       <StudentFileTabs />
+      <StudentProfileCenter />
     </>
   );
 }
