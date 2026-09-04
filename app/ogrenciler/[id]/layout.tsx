194d48592536f4a06f8dac47f49f1a29eb9080f4
SPRINTYUZMEOKULU <sprintyuzmeokulu@gmail.com>
2026-09-04 14:10:19 +0300

    Öğrenci hızlı işlem butonlarını doğru merkezlere bağla

--- app/ogrenciler/[id]/layout.tsx
+++ app/ogrenciler/[id]/layout.tsx
@@ -1,10 +1,10 @@
 import type { ReactNode } from "react";
 import StudentFileTabs from "./student-file-tabs";
 import StudentProfileCenter from "./student-profile-center";
-import ProfileCenterClickBridge from "./profile-center-click-bridge";
 import GeneralInfoSummary from "./general-info-summary";
 import StudentRenewalCenter from "./student-renewal-center";
 import StudentPaymentCenter from "./student-payment-center";
+import StudentActionRouter from "./student-action-router";
 import StudentActionFeedback from "./student-action-feedback";
 import StudentHeroEnhancer from "./student-hero-enhancer";
 import RenewalApprovalOpenBridge from "./renewal-approval-open-bridge";
@@ -18,10 +18,10 @@ export default function StudentFileLayout({ children }: { children: ReactNode })
       {children}
       <StudentFileTabs />
       <StudentProfileCenter />
-      <ProfileCenterClickBridge />
       <GeneralInfoSummary />
       <StudentRenewalCenter />
       <StudentPaymentCenter />
+      <StudentActionRouter />
       <StudentActionFeedback />
       <StudentHeroEnhancer />
       <RenewalApprovalOpenBridge />
