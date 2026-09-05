94aeb61bd703fde631ae4fb85f8241b5ff320ed9
SPRINTYUZMEOKULU <sprintyuzmeokulu@gmail.com>
2026-09-06 01:19:40 +0300

    fix(payments): mount student payment center in student layout

--- app/ogrenciler/[id]/layout.tsx
+++ app/ogrenciler/[id]/layout.tsx
@@ -4,6 +4,7 @@ import StudentFileTabs from "./student-file-tabs";
 import StudentProfileCenter from "./student-profile-center";
 import GeneralInfoSummary from "./general-info-summary";
 import StudentRenewalCenter from "./student-renewal-center";
+import StudentPaymentCenter from "./student-payment-center";
 import StudentActionRouter from "./student-action-router";
 import StudentActionFeedback from "./student-action-feedback";
 import StudentHeroEnhancer from "./student-hero-enhancer";
@@ -38,6 +39,7 @@ export default async function StudentFileLayout({
       <StudentProfileCenter />
       <GeneralInfoSummary />
       <StudentRenewalCenter />
+      <StudentPaymentCenter />
       <StudentActionRouter />
       <StudentActionFeedback />
       <StudentHeroEnhancer />
